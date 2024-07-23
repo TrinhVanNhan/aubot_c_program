4dbccbf2a448295c487302498d62dfc486030587
Tung1308@ <huutung1308@gmail.com>
2024-07-23 11:23:12 +0700

    ...

--- bai_tap_cap_phat_dong.c
+++ bai_tap_cap_phat_dong.c
@@ -13,6 +13,44 @@ uint8_t *cap_phat(uint8_t *pt,frame_t frame)
 	pt = (uint8_t*)(calloc(frame.len +3,sizeof(uint8_t)));
 	return pt;
 }
+void tach_so(char pt[], uint8_t n, uint8_t dll[])
+{
+	char data[50]="";
+	char *ptt = strtok(pt,"|");
+	while(ptt)
+	{
+		strcat(data,ptt);
+		ptt = strtok(NULL,"|");
+	}
+	printf("%s",data);
+	printf("\n");
+	char *pttt = strtok(data,"0x");
+	uint8_t dl[50]={};
+	int i=0;
+	char *p;
+	while(pttt)
+	{
+		dl[i] = strtol(pttt,&pttt,16);
+		pttt = strtok(NULL,"0x");
+		i++;
+	}
+	int x=0;
+	for(;x<n;x++)
+	{
+		dll[x]=dl[x];
+	}
+}
+uint8_t check_sum(uint8_t pt[], uint8_t len)
+{
+	uint8_t sum = 0;
+	int i=0;
+	for(;i<len;i++)
+	{
+		sum = sum + pt[i];
+	}
+	sum = sum%256;
+	return sum;
+}
 void copy_data(uint8_t pt[], uint8_t *ptt ,frame_t frame)
 {
 	frame.len = pt[1];
@@ -29,13 +67,19 @@ void copy_data(uint8_t pt[], uint8_t *ptt ,frame_t frame)
 	{
 		printf("|%x",ptt[j]);
 	}
+	printf("\n");
+	uint8_t check = check_sum(&ptt[2], frame.len);
+	printf("check_sum bang %x",check);
+	free(ptt);
 }
 
 int main()
 {
 	frame_t frame1 ;
 	uint8_t *ptt = NULL;
-	uint8_t dulieu[8] = {0x98,0x05,0x12,0x01,0x06,0xAC,0x04,0x99};
+	uint8_t dl[50] = "|0x98|0x05|0x12||0x01||0x06||0xAC||0x04||0x99|";
+	uint8_t dulieu[]={};
+	tach_so(dl,8,dulieu);
 	copy_data(dulieu,ptt,frame1);
 	return 0;
 }
